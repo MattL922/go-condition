280b614955b2f020be1f85736a7f5c5aa4c08db3
Matthew Loppatto <mattloppatto@gmail.com>
2018-10-10 16:44:48 -0400

    Added Condition type and utility functions

---
+++ condition.go
@@ -0,0 +1,31 @@
+package condition
+
+// Condition is a function that evaluates if a condition is true or false.
+type Condition func() bool
+
+// All returns true only if all of the provided conditions are true.
+func All(conditions ...Condition) bool {
+    for _, condition := range conditions {
+        if !condition() { return false }
+    }
+    return true
+}
+
+// Any returns true if at least one of the provided conditions is true, false
+// otherwise.
+func Any(conditions ...Condition) bool {
+    for _, condition := range conditions {
+        if condition() { return true }
+    }
+    return false
+}
+
+// None returns true if none of the provided conditions are true, false
+// otherwise.
+func None(conditions ...Condition) bool {
+    for _, condition := range conditions {
+        if condition() { return false }
+    }
+    return true
+}
+
